516dc175148c73a0b7234c79e3135ecd844b6b53
roberto <robaraneda@gmail.com>
2020-04-19 17:14:59 -0400

    se agregan componentes base para ser reutilizados

---
+++ src/components/Base/Autocomplete.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-autocomplete
+    color="blueS"
+    dense
+    v-bind="$attrs"
+    v-on="$listeners"
+  ></v-autocomplete>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false
+}
+</script>
---
+++ src/components/Base/Textfield.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-text-field
+    color="blueS"
+    dense
+    v-bind="$attrs"
+    v-on="$listeners"
+  ></v-text-field>
+</template>
+<script>
+export default {
+  inheritAttrs: false
+}
+</script>
