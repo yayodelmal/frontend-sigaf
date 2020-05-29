ce451ccb1490231e4713efaf3e14e275ea2cc8a5
claudia_alarcon <c.alarconlazo@gmail.com>
2020-05-29 19:24:11 -0400

    modificación archivo motiveTicket.js en store

--- src/store/modules/motiveTicket.js
+++ src/store/modules/motiveTicket.js
@@ -1,35 +1,47 @@
 import axios from '../../services/axios'
 
+const BASE_URL = '/api/v2/motive-tickets'
+
 export default {
+  namespaced: true,
   state: {
-    motiveTickets: []
+    motiveTickets: [],
+    tikets: []
   },
   mutations: {
     SET_MOTIVE_TICKETS: (state, motiveTickets) => {
       state.motiveTickets = motiveTickets
+    },
+    SET_TICKET: (state, tickets) => {
+      state.tickets = tickets
     },
     POST_MOTIVE_TICKET: (state, motiveTicket) => {
       state.motiveTickets.push(motiveTicket)
     },
     PUT_MOTIVE_TICKET: (state, motiveTicket) => {
       const editedIndex = state.motiveTickets.findIndex(
-        find => find.id === motiveTicket.id
+        find => find.properties.id === motiveTicket.properties.id
       )
+
       Object.assign(state.motiveTickets[editedIndex], motiveTicket)
     },
     DELETE_MOTIVE_TICKET: (state, motiveTicket) => {
       const editedIndex = state.motiveTickets.findIndex(
-        find => find.id === motiveTicket.id
+        find => find.properties.id === motiveTicket.id
       )
       state.motiveTickets.splice(editedIndex, 1)
     }
   },
   getters: {
-    motiveTicketsDataBase: state => {
-      return state.motiveTickets.map(motiveTicket => {
+    motiveTickets: state => {
+      return state.motiveTickets.map(({ properties, relationships }) => {
         return {
-          id: motiveTicket.id,
-          description: motiveTicket.description
+          id: properties.id,
+          description: properties.description,
+          tickets: {
+            numberOfElements: relationships.numberOfElements,
+            href: relationships.links.href
+          }
         }
       })
     }
@@ -37,9 +49,39 @@ export default {
   actions: {
     fetchMotiveTickets: async ({ commit }) => {
       try {
-        const { data } = await axios.get('motive-ticket')
-        commit('SET_MOTIVE_TICKETS', data.motiveTickets)
-        return { success: data.success, error: data.error }
+        const { data } = await axios.get(BASE_URL)
+
+        const { _data, success, error, message } = data
+
+        if (success) {
+          commit('SET_MOTIVE_TICKETS', _data.collections)
+        } else {
+          console.log(error)
+        }
+
+        return { success, message }
+      } catch (error) {
+        const { data } = error.response
+        console.log(error)
+        return {
+          success: data.success,
+          message: data.message
+        }
+      }
+    },
+    fetchTickets: ({ commit, state }, idMotiveTicket) => {
+      try {
+        state.motiveTickets.forEach(async ({ properties, relationships }) => {
+          if (properties.id === idMotiveTicket) {
+            const { data } = await axios.get(relationships.links.href)
+
+            const { _data, success, error } = data
+
+            commit('SET_TICKETS', _data.relationships.collection.data)
+
+            return { success, error }
+          }
+        })
       } catch (error) {
         const { data } = error.response
         return {
@@ -50,9 +92,19 @@ export default {
     },
     postMotiveTicket: async ({ commit }, motiveTicket) => {
       try {
-        const { data } = await axios.post('motive-ticket', motiveTicket)
-        commit('POST_MOTIVE_TICKET', data.motiveTicket)
-        return { success: data.success, error: data.error }
+        const { data } = await axios.post(BASE_URL, motiveTicket)
+
+        console.log(motiveTicket)
+
+        const { _data, success, error, message } = data
+
+        if (success) {
+          commit('POST_MOTIVE_TICKET', _data)
+        } else {
+          console.log(error)
+        }
+
+        return { success, message }
       } catch (error) {
         const { data } = error.response
         return {
@@ -64,12 +116,19 @@ export default {
     putMotiveTicket: async ({ commit }, motiveTicket) => {
       try {
         const { data, status } = await axios.put(
-          `motive-ticket/${motiveTicket.id}`,
+          `${BASE_URL}/${motiveTicket.id}`,
           motiveTicket
         )
         if (status === 200) {
-          commit('PUT_MOTIVE_TICKET', data.motiveTicket)
-          return { success: data.success, error: data.error }
+          const { _data, success, error, message } = data
+
+          if (success) {
+            commit('PUT_MOTIVE_TICKET', _data)
+          } else {
+            console.log(error)
+          }
+
+          return { success, message }
         } else {
           return {
             success: data.success,
@@ -87,12 +146,19 @@ export default {
     deleteMotiveTicket: async ({ commit }, motiveTicket) => {
       try {
         const { status, data } = await axios.delete(
-          `motive-ticket/${motiveTicket.id}`
+          `${BASE_URL}/${motiveTicket.id}`
         )
 
         if (status === 200) {
-          commit('DELETE_MOTIVE_TICKET', motiveTicket)
-          return { success: data.success, error: data.error }
+          const { success, error, message } = data
+
+          if (success) {
+            commit('DELETE_MOTIVE_TICKET', motiveTicket)
+          } else {
+            console.log(error)
+          }
+
+          return { success, message }
         } else {
           return {
             success: data.success,
@@ -100,9 +166,9 @@ export default {
           }
         }
       } catch (error) {
-        const { data } = error.response
+        console.log(error)
         return {
-          success: data.success,
+          success: false,
           error: 'Error grave. Contacte al Administrador.'
         }
       }
