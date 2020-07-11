1da5dc5995a9832eaefa42c9f462cfbbdeec7c3d
claudia_alarcon <c.alarconlazo@gmail.com>
2020-07-11 00:25:23 -0400

    creación User.vue, modificación de index en router y NavigationDrawer en my

---
+++ src/views/maintenance/User.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <base-card color="blueS" class="px-5 py-3" title="Usuario"> </base-card>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
