f52143c453b1d12a7863f98a70496756f2c14c44
claudia_alarcon <c.alarconlazo@gmail.com>
2020-07-12 23:44:21 -0400

    actualización de constructor de usuario

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,25 @@
+import Role from './Role'
+
 export default class User {
-  constructor(email, password) {
+  constructor(
+    id,
+    rut,
+    name,
+    phone,
+    mobile,
+    email,
+    role = new Role(),
+    password = 'sigaf',
+    isFirstLogin = 0
+  ) {
+    this.id = id
+    this.rut = rut
+    this.name = name
+    this.phone = phone
+    this.mobile = mobile
     this.email = email
+    this.role = role
     this.password = password
+    this.isFirstLogin = isFirstLogin
   }
 }
