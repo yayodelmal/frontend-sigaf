dae5fe970a415fab02a88bef03888c771499aa67
claudia_alarcon <c.alarconlazo@gmail.com>
2021-01-10 18:56:45 -0300

    creación de archivo sigafDatatable reemplazo de componente datatable local por componente sigaf-datatable

--- src/views/maintenance/Category.vue
+++ src/views/maintenance/Category.vue
@@ -11,66 +11,17 @@
         :transition="transition"
         :loading="loading"
       ></sigaf-skeleton-loader>
-
-      <v-data-table
-        :search="search"
+      <sigaf-datatable
         v-else
-        :headers="headers"
         :items="categoriesItems"
-        :items-per-page="5"
+        :headers="headers"
+        :buttonName="buttonName"
         :loading="loading"
-        class="elevation-1"
-        loading-text="Cargando... por favor espere"
-      >
-        <template v-slot:progress>
-          <v-progress-linear
-            color="blueS"
-            :height="3"
-            indeterminate
-          ></v-progress-linear>
-        </template>
-        <template v-slot:top>
-          <v-toolbar tile dark color="blueS darken-1" class="mb-1">
-            <v-text-field
-              v-model="search"
-              color="blueS"
-              clearable
-              flat
-              solo-inverted
-              hide-details
-              prepend-inner-icon="mdi-magnify"
-              label="Buscar"
-            ></v-text-field>
-            <v-spacer></v-spacer>
-            <v-btn depressed large color="blueS" @click="createCategory">
-              <v-icon class="mr-2" size="25">mdi-plus</v-icon>
-              Crear Categoría
-            </v-btn>
-          </v-toolbar>
-        </template>
-        <template v-slot:item.actions="{ item }">
-          <v-tooltip color="blueS" bottom>
-            <template v-slot:activator="{ on }">
-              <v-btn icon text v-on="on">
-                <v-icon @click.prevent="editItem(item)">
-                  mdi-pencil
-                </v-icon>
-              </v-btn>
-            </template>
-            <span>Editar</span>
-          </v-tooltip>
-          <v-tooltip color="blueS" bottom>
-            <template v-slot:activator="{ on }">
-              <v-btn icon text v-on="on">
-                <v-icon @click.prevent="deleteItem(item)">
-                  mdi-delete
-                </v-icon>
-              </v-btn>
-            </template>
-            <span>Eliminar</span>
-          </v-tooltip>
-        </template>
-      </v-data-table>
+        :itemsPerPage="5"
+        @createItem="createCategory"
+        @editItem="editItem($event)"
+        @deleteItem="deleteItem($event)"
+      ></sigaf-datatable>
     </base-card>
     <v-dialog v-model="dialog" max-width="500px" persistent>
       <v-form>
@@ -171,6 +122,7 @@ import SigafSnackbar from '../../components/component/Snackbar'
 import { Snackbar } from '../../utils/constants'
 import ConfirmDialog from '../../components/component/ConfirmCard'
 import SigafSkeletonLoader from '../../components/maintenance/SigafSkeletonLoader.vue'
+import SigafDatatable from '../../components/maintenance/SigafDatatable.vue'
 
 export default {
   inject: ['theme'],
@@ -178,7 +130,8 @@ export default {
   components: {
     SigafSnackbar,
     ConfirmDialog,
-    SigafSkeletonLoader
+    SigafSkeletonLoader,
+    SigafDatatable
   },
   validations: {
     description: {
@@ -198,6 +151,7 @@ export default {
   data: () => ({
     dialog: false,
     dialogConfirm: false,
+    buttonName: 'Crear categoría',
     headers: [
       {
         text: 'Nombre',
@@ -320,6 +274,7 @@ export default {
       this.makeSnakResponse(Snackbar.ERROR.message, Snackbar.ERROR.type)
     },
     editItem(item) {
+      console.log('mostrando item en el papá', item)
       this.getPlatforms()
       this.editedIndex = this.categoriesItems.indexOf(item)
 
