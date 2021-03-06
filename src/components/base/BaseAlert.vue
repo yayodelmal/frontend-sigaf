d7ec1d29a71ad9772b8e0ea782afba6ea0323ba4
Roberto Araneda Espinoza <robaraneda@gmail.com>
2021-03-06 17:06:29 -0300

    se crea base alert

---
+++ src/components/base/BaseAlert.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-alert
+    :value="true"
+    @input="$emit('input', $event)"
+    dismissible
+    color="cyan"
+    border="left"
+    elevation="2"
+    colored-border
+    icon="mdi-twitter"
+    transition="scale-transition"
+  >
+    You've got <strong>5</strong> new updates on your timeline!.
+  </v-alert>
+</template>
+
+<script>
+export default {
+  name: 'BaseAlert',
+  props: {
+    value: Boolean
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
